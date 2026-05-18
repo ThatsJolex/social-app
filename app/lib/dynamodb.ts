49e9e2dbe71934f259e7b72efa24401f3d40e891
ThatsJolex <azolakeliwe12@gmail.com>
2026-05-18 12:31:21 +0200

    Override the endpoint so that we connect to the LOCAL DynamoDB instead of the one AWS SDK normally connects to

--- app/lib/dynamodb.ts
+++ app/lib/dynamodb.ts
@@ -1,6 +1,16 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb"
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb"
 
-export const dynamo = new DynamoDBClient({
-  region: "local",
-  endpoint: "http://localhost:8000",
+const client = new DynamoDBClient({
+  region: process.env.AWS_REGION,
+
+  endpoint: process.env.DYNAMODB_ENDPOINT,
+
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "fakeMyKeyId",
+    secretAccessKey:
+      process.env.AWS_SECRET_ACCESS_KEY || "fakeSecretAccessKey",
+  },
 })
+
+export const dynamodb = DynamoDBDocumentClient.from(client)

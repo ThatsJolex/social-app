c27e113245a815941b98ceba0d02928833963784
ThatsJolex <azolakeliwe12@gmail.com>
2026-05-18 11:25:54 +0200

    new file:  app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <input placeholder="Email" />
+      <input placeholder="Password" type="password" />
+      <button>Login</button>
+    </div>
+  )
+}

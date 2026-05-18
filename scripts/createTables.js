385dcaf08c7cca8934f6f06b257fa1ea4e789dd0
ThatsJolex <azolakeliwe12@gmail.com>
2026-05-18 12:23:44 +0200

    Sending commands to DynamoDB that each table needs a name and a primary key

---
+++ scripts/createTables.js
@@ -0,0 +1,66 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const {
+  DynamoDBClient,
+  CreateTableCommand,
+} = require("@aws-sdk/client-dynamodb")
+
+const client = new DynamoDBClient({
+  region: "local",
+  endpoint: "http://localhost:8000",
+})
+
+async function createTables() {
+  try {
+    // USERS TABLE
+    await client.send(
+      new CreateTableCommand({
+        TableName: "Users",
+        AttributeDefinitions: [
+          { AttributeName: "userId", AttributeType: "S" },
+        ],
+        KeySchema: [
+          { AttributeName: "userId", KeyType: "HASH" },
+        ],
+        BillingMode: "PAY_PER_REQUEST",
+      })
+    )
+
+    console.log("Users table created")
+
+    // POSTS TABLE
+    await client.send(
+      new CreateTableCommand({
+        TableName: "Posts",
+        AttributeDefinitions: [
+          { AttributeName: "postId", AttributeType: "S" },
+        ],
+        KeySchema: [
+          { AttributeName: "postId", KeyType: "HASH" },
+        ],
+        BillingMode: "PAY_PER_REQUEST",
+      })
+    )
+
+    console.log("Posts table created")
+
+    // FOLLOWS TABLE
+    await client.send(
+      new CreateTableCommand({
+        TableName: "Follows",
+        AttributeDefinitions: [
+          { AttributeName: "followId", AttributeType: "S" },
+        ],
+        KeySchema: [
+          { AttributeName: "followId", KeyType: "HASH" },
+        ],
+        BillingMode: "PAY_PER_REQUEST",
+      })
+    )
+
+    console.log("Follows table created")
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+createTables()

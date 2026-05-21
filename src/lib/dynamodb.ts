2925731397178e55b94125d39d0c0f5d467ecfd0
ThatsJolex <azolakeliwe12@gmail.com>
2026-05-21 12:20:27 +0200

    Fix DynamoDB config for Vercel

--- src/lib/dynamodb.ts
+++ src/lib/dynamodb.ts
@@ -4,13 +4,15 @@ import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb"
 const client = new DynamoDBClient({
   region: process.env.AWS_REGION,
 
-  endpoint: process.env.DYNAMODB_ENDPOINT,
-
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "fakeMyKeyId",
-    secretAccessKey:
-      process.env.AWS_SECRET_ACCESS_KEY || "fakeSecretAccessKey",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
+
+  ...(process.env.NODE_ENV === "development" && {
+    endpoint: "http://localhost:8000",
+  }),
 })
 
-export const dynamodb = DynamoDBDocumentClient.from(client)
+export const dynamodb =
+  DynamoDBDocumentClient.from(client)
